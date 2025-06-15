4b97ef36c0120afe7e7dbe0487b4f75ada7fb3e4
Hacker2999 <wwwhuker.mafka@gmail.com>
2025-06-15 13:35:16 +0300

    fix anekdot

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ def quota_check(userid: int, qcount: int) -> bool:
     if recent_time.tzinfo is None:
         recent_time = recent_time.replace(tzinfo=timezone.utc)
     days_passed = (now - recent_time).days
-    if qcount >= 3 and days_passed >= 1:
+    if days_passed >= 1:
         q2 = (
             AnekModel
             .update({AnekModel.count: 0})
